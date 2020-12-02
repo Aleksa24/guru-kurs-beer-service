9b4b119cfa60a3cb20c638e53f24c425f8e677e3
Aleksa24 <aleksaivanovic24444@gmail.com>
2020-12-02 13:36:26 +0100

    jms added

--- src/main/java/guru/sfg/common/events/BeerEvent.java
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -1,21 +1,17 @@
-package guru.springframework.msscbeerservice.events;
+package guru.sfg.common.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class BeerEvent implements Serializable {
 
     static final long serialVersionUID = 5925166867424343040L;
 
-    private final BeerDto beerDto;
-
-
-
+    private BeerDto beerDto;
 }
--- src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
+++ src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
@@ -1,9 +1,8 @@
 package guru.springframework.msscbeerservice.services.brewing;
 
-import guru.springframework.msscbeerservice.config.JmsConfig;
 import guru.springframework.msscbeerservice.domain.Beer;
-import guru.springframework.msscbeerservice.events.BrewBeerEvent;
-import guru.springframework.msscbeerservice.events.NewInventoryEvent;
+import guru.sfg.common.events.BrewBeerEvent;
+import guru.sfg.common.events.NewInventoryEvent;
 import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import static guru.springframework.msscbeerservice.config.JmsConfig.BREWING_REQUEST_QUEUE;
 import static guru.springframework.msscbeerservice.config.JmsConfig.NEW_INVENTORY_QUEUE;
@@ -23,8 +23,9 @@ public class BrewBeerListener {
     private final BeerRepository beerRepository;
     private final JmsTemplate jmsTemplate;
 
+    @Transactional
     @JmsListener(destination = BREWING_REQUEST_QUEUE)
-    private void listen(BrewBeerEvent brewBeerEvent){
+    public void listen(BrewBeerEvent brewBeerEvent){
         BeerDto beerDto = brewBeerEvent.getBeerDto();
 
         Beer beer = beerRepository.getOne(beerDto.getId());
